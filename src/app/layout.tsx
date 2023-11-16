c868143a3447e862d9f71c28269f420a10fbd88d
aqilmarwan <aqilmarwanazhar@gmail.com>
2023-11-16 21:47:40 +0800

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from '@/components/Navbar';
 import Providers from '@/components/Providers';
 import { cn, constructMetadata } from '@/lib/utils';
@@ -30,6 +31,7 @@ export default function RootLayout({
           <Toaster />
           <Navbar />
           {children}
+          <Analytics />
         </body>
       </Providers>
     </html>
