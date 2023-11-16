19a5cbcfa5514259f08f77c27a7a626f5ff80680
aqilmarwan <aqilmarwanazhar@gmail.com>
2023-11-16 21:31:28 +0800

    first commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,49 @@
+import { type ClassValue, clsx } from 'clsx';
+import { Metadata } from 'next';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function absoluteUrl(path: string) {
+  if (typeof window !== 'undefined') return path;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
+
+export function constructMetadata({
+  title = 'Fraser - the SaaS for students',
+  description = 'Fraser is an open-source software to make chatting to your PDF files easy.',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+    },
+    icons,
+    metadataBase: new URL('https://quill-jet.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
